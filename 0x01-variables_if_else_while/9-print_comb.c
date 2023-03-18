1a9a023c3f100ce9fd170cca37ade49ec522d2d6
Chidobe <thedobecompany@gmail.com>
2023-03-18 08:10:36 -0700

    numbers with spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,21 +6,13 @@
 int main(void)
 {
 int c;
-int i;
-int s;
 for (c = 0; c < 10; c++)
 {
 putchar('0' + c);
 if (c == 9)
 break;
-for (i = c; i < c; i++)
-{
 putchar(',');
-for (s = c; s < c; s++)
-{
 putchar(' ');
-}
-}
 }
 putchar('\n');
 return (0);
