d26890d6e3dd3a68d2492a21e11fd1670181ea18
Chidobe <thedobecompany@gmail.com>
2023-03-19 07:08:09 -0700

    combo of 2 two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+* main - program the prints combination of two 2-digit numbers from 0 - 9
+* Return: 0
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+for (i = 00; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 10; k++)
+{
+for (l = 1; l < 10; l++)
+{
+if (i >= j)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+if (i == 9 && j == 8 && k == 9 && l == 9)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
