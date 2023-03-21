f56fb13627c966ababe87445c2eef5d83377a1a4
Chidobe <thedobecompany@gmail.com>
2023-03-21 16:05:38 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,12 @@
  * Return: 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void)
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		char i;
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
-	return (0);
+	_putchar('\n');
 }
