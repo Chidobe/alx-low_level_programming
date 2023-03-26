a9f32bbfe2bf894b59b5b9cae894184c46a5dcea
Chidobe <thedobecompany@gmail.com>
2023-03-26 13:36:15 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - checks if character is between 0 - 9.
+* @a: is operand
+* @b: is operand
+* Return: result of multiplication.
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
