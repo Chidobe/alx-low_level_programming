c9eda452c13eeabf35226223a24369bd2d8aa7f9
Chidobe <thedobecompany@gmail.com>
2023-03-26 13:56:34 -0700

    count

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
 * print_numbers - prints 0-9.
-* Return: (0).
 */
 
 void print_numbers(void)
@@ -13,5 +12,5 @@ for (i = 0; i < 10; i++)
 {
 _putchar(i);
 }
-_putchar("\n");
+_putchar('\n');
 }
