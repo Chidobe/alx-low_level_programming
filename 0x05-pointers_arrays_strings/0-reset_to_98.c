aec227c3224d22c9be0dd09c00c91bf7668948f6
Chidobe <thedobecompany@gmail.com>
2023-03-31 11:52:19 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates pointer value pointed to
+ * @*n: pointer parameter
+ * Return: 0 always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
