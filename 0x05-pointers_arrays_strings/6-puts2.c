5b8e50a6e08cc67ee9d1adcfa478f403cd57e264
Chidobe <thedobecompany@gmail.com>
2023-04-02 07:19:31 -0700

    everyother

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other character
+ *@str: the string
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str+2);
+	}
+	_putchar('\n');
+}
