99c29d2389b56195258a0ac34a60fbf707ff3cac
Chidobe <thedobecompany@gmail.com>
2023-04-02 01:49:43 -0700

    prints the string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strlen - outputs the length of a string
+ *@s: the string
+ *Return: the string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (i > 0)
+	{
+		if  (s[i] == 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
