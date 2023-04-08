0d07845e380210f806fe8e25bdf86d60d23eb249
Chidobe <thedobecompany@gmail.com>
2023-04-08 12:26:24 -0700

    print struing

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != 0)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		*s++;
-		_puts_recursion(char *s);
+		return;
 	}
 	else
 	{
-		return;
+		_putchar(s);
+		s++;
+		_puts_recursion(char *s);
 	}
 	_putchar('\n');
 }
