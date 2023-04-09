55cc48bce140c4d8bd8c8fe8158272f148e9a7ed
Chidobe <thedobecompany@gmail.com>
2023-04-08 23:10:43 -0700

    print string with ree=cursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,6 +10,7 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
+		_putchar('\n');
 	}
 	else
 	{
@@ -17,5 +18,4 @@ void _puts_recursion(char *s)
 		s++;
 		_puts_recursion(s);
 	}
-	_putchar('\n');
 }
