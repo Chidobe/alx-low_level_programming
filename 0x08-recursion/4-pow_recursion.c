dd8351f1cef23a4014e69eabf3efc81d082d9950
Chidobe <thedobecompany@gmail.com>
2023-04-09 04:47:25 -0700

    recursion powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - returns value of x to power of y
+ *@x: value
+ *@y: power
+ *Return: the value of x to power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
