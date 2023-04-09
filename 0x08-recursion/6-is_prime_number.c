5a38314f9d1a3f0fece6f29bfb3134de913dfa22
Chidobe <thedobecompany@gmail.com>
2023-04-09 09:29:12 -0700

    check of a number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,13 +3,43 @@
 /**
  *is_prime_number - returns 1 if prime
  *@n: number
+ *@c: counter
  *Return: 1 if prime
  */
 
+int calc_prime(int n, int c);
+
 int is_prime_number(int n)
 {
-	if (n / n)
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (calc_prime(n, n - 1));
+	}
+}
+
+/**
+ * calc_prime - calculates the prime recursively
+ * @n: number
+ * @c: counter
+ * Return: 1 if prime
+ */
+
+int calc_prime(int n, int c)
+{
+	if (c == 1)
 	{
 		return (1);
+	}
+	else if (n % c == 0 && c > 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (calc_prime(n, c - 1));
 	}
 }
