5ba0d4eb597ddfebf5b4c87f8e3a6a937033a465
Chidobe <thedobecompany@gmail.com>
2023-04-10 01:17:52 -0700

    adding all files

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@: the string
+ *@accept: the bytes to be used
+ *Return: the length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
