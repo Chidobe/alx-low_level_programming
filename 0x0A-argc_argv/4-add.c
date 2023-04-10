74e249a13c08676ebd4bdecde8f16d5b000b59a3
Chidobe <thedobecompany@gmail.com>
2023-04-10 14:44:52 -0700

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - prints addition
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: the sum
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum = sum + atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+}
