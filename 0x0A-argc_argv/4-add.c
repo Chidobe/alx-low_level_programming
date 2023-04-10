e2d5c8a9d13e7b99015c581e1458855cb94dc1b1
Chidobe <thedobecompany@gmail.com>
2023-04-10 14:49:19 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,11 +22,13 @@ int main(int argc, char *argv[])
 	for (a = 1; a < argc; a++)
 	{
 		for (b = 0; argv[a][b] != '\0'; b++)
+		{
 			if (!isdigit(argv[a][b]))
 			{
 				printf("Error\n");
 				return (1);
 			}
+		}
 		sum = sum + atoi(argv[a]);
 	}
 	printf("%d\n", sum);
