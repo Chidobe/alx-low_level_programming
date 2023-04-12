0353439329c30644c9a253298ee9edf65585f50d
Chidobe <thedobecompany@gmail.com>
2023-04-12 13:26:15 -0700

    mallo create arrays

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - prints addition
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: the sum
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+}
---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *create_array - function that creates an array of characters
+ *@size: size to be allocated
+ *@c: character to be put
+ *Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int *charr;
+	int i;
+}
