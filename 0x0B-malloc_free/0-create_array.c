cb2a1296f2ca010024f8d4c325ce075c8c2d501a
Chidobe <thedobecompany@gmail.com>
2023-04-12 13:46:00 -0700

    first draft of code to create aray

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,4 +12,20 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int *charr;
 	int i;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	else 
+	{
+		charr = malloc(sizeof(unsigned int) * size);
+
+		for (i = 0; i < size; i++)
+		{
+			charr[i] = c;
+		}
+		return (charr);
+	}
+	return (0);
 }
