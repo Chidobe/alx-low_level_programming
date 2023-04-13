c4ee62261bf964001a0ed9394027961a95ee913e
Chidobe <thedobecompany@gmail.com>
2023-04-13 01:45:59 -0700

    function to return pointer with copy of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - function that returns pointer to allocated memory contaiinig string
+ *@str: the string
+ *Return: pointer to the string
+ */
+
+char *_strdup(char *str)
+{
+	char *pstr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else 
+	{
+	pstr = malloc(sizeof(char));
+	*pstr = str;
+	return (*pstr);
+	}
+}
