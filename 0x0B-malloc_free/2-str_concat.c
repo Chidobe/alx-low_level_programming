1069b54328365fada0f25770162ac1ca46e1ef5d
Chidobe <thedobecompany@gmail.com>
2023-04-13 01:54:23 -0700

    function ot concatenate strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_str_concat - function to concatenate strings
+ *@s1: first string
+ *@s2: second string
+ *Return: pointer to concatenated string
+ */
+
+char *_str_concat(char *s1, char *s2)
+{
+}
